99b07ea272d3f17d36d7fbfa994029b5587d7a3c
lauhpnx <laura62251336@alunos.sc.senac.br>
2026-05-06 14:11:58 -0300

    inicio  cena

--- github/Assets/Scripts/StartGame.cs
+++ github/Assets/Scripts/StartGame.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void IniciarJogo()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("fase1"); // nome da sua cena
     }
 }

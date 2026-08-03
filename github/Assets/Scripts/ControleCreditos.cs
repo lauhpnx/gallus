ea379205b0e8901779e376097c85a10a873e73e2
lauhpnx <laura62251336@alunos.sc.senac.br>
2026-08-03 16:40:21 -0300

    ajustes painel creditos

---
+++ github/Assets/Scripts/ControleCreditos.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ControleCreditos : MonoBehaviour
+
+{
+    public GameObject painelCreditos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        painelCreditos.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void PainelCreditosAberto()
+    {
+        painelCreditos.SetActive(true);
+    }
+
+    public void PainelCreditosFechado()
+    {
+        painelCreditos.SetActive(false);
+    }
+}

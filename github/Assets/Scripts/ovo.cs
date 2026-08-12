402066eaf16079c34d601c35b3e64a14d34fc249
lauhpnx <laura62251336@alunos.sc.senac.br>
2026-08-12 15:52:04 -0300

    ajustes no script do ovo

--- github/Assets/Scripts/ovo.cs
+++ github/Assets/Scripts/ovo.cs
@@ -8,23 +8,21 @@ public class ovo : MonoBehaviour
 
     public GameObject explosaoPrefab;
 
+    void morrer()
+    {
+        if (explosaoPrefab != null)
+        {
+            Instantiate(explosaoPrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Corrigido: Agora checa se o objeto que tomou o tiro (collision) é o Galo
         if (collision.CompareTag("Galo"))
         {
-            Instantiate(explosaoPrefab, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-            Instantiate(
-
-                explosaoPrefab,
-                transform.position,
-                Quaternion.identity
-
-             );
-
-            // Destrói apenas o ovo. O script de vida do Galo cuida do resto!
-            Destroy(gameObject);
+            morrer();
         }
 
     }

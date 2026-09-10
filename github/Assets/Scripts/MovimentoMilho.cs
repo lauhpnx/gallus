560b85c825e5a32532166e37ce59538846975433
lauhpnx <laura62251336@alunos.sc.senac.br>
2026-09-10 14:10:23 -0300

    ajustes nas fases 1 e 2

--- github/Assets/Scripts/MovimentoMilho.cs
+++ github/Assets/Scripts/MovimentoMilho.cs
@@ -10,7 +10,6 @@ public class MovimentoMilho : MonoBehaviour
         transform.Translate(Vector3.down * velocidade * Time.deltaTime, Space.World);
 
         // Se o milho passar da parte de baixo da tela, ele destrói-se para não pesar o jogo
-        // Se a sua câmara for muito grande, altere o -10f para um número menor (ex: -15f)
         if (transform.position.y < -10f)
         {
             Destroy(gameObject);

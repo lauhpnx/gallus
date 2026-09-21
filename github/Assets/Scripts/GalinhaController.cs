029ed36b25fad359a747d5bbd8464011a69682db
ThiegoDeAlbuquerque <thiegodealbuquerque06@gmail.com>
2026-09-21 16:31:23 -0300

    arrumando o aviso, arrumando o script da galinha para quando coliddir dar 2 ovos e arrumando o tamanho das galinhas

--- github/Assets/Scripts/GalinhaController.cs
+++ github/Assets/Scripts/GalinhaController.cs
@@ -7,7 +7,7 @@ public class GalinhaController : MonoBehaviour
 {
     // Referência única da galinha, pra outros scripts (como o "ovo") acessarem sem precisar procurar na cena
     public static GalinhaController Instance;
-
+    public ovo ovoScript;
     [Header("Configurações de Vida")]
     public int life = 10;
     public int _lifemax = 10;
@@ -215,5 +215,14 @@ public class GalinhaController : MonoBehaviour
     {
      ovosportiro = tipo;
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Milho"))
+        {
+            
+            Destroy(collision.gameObject);
+            AdicionarOvos(2);
+        }
     }
 }
